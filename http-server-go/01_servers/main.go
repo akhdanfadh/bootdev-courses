93c47addc9687666bf4632730ae08052bb8b1c49
Akhdan Fadhilah <me@akhdan.dev>
2025-08-07 05:29:21 +0900

    [srv-go] add simple fileserver on cwd

--- http-server-go/01_servers/main.go
+++ http-server-go/01_servers/main.go
@@ -6,6 +6,11 @@ func main() {
 	// A multiplexer is responsible for routing HTTP requests to appropriate handler
 	mux := http.NewServeMux()
 
+	// A simple fileserver on current directory (./index.html)
+	fileServerHandler := http.FileServer(http.Dir("."))
+	// a url with pattern /app/ will directed to the fileserver (which is cwd)
+	mux.Handle("/", fileServerHandler)
+
 	// A simple way to run HTTP server with configured parameters
 	server := http.Server{
 		Handler: mux,
