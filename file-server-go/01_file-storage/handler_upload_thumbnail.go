10aa1a3775672303f8569b001694b965a8bbdc96
Akhdan Fadhilah <me@akhdan.dev>
2025-08-09 10:41:48 +0900

    [fsrv-go] add thumbnail upload handling with multipart form

--- file-server-go/01_file-storage/handler_upload_thumbnail.go
+++ file-server-go/01_file-storage/handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,60 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	// START: akhdanfadh's implementation of multipart form uploads
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// Parse the multipart form data into the request object itself
+	// with large enough memory to hold in RAM, rest will be stored in a temporary file
+	const maxMemory = 10 << 20 // left bit shift; 10 in binary is 1010, so 1010 with 20 trailing zeros (10MB)
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse multipart form", err)
+		return
+	}
+
+	// Get image data from the form
+	tnFile, tnHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get thumbnail file from form", err)
+		return
+	}
+	tnType := tnHeader.Header.Get("Content-Type")
+
+	// Read all image data into a byte slice
+	tnData, err := io.ReadAll(tnFile)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't read thumbnail file", err)
+		return
+	}
+
+	// Get the video metadata from the database
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't get video", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You are not allowed to upload thumbnail for this video", nil)
+		return
+	}
+
+	// Save the thumbnail to the global map
+	videoThumbnails[videoID] = thumbnail{
+		data:      tnData,
+		mediaType: tnType,
+	}
+
+	// Update the video metadata for the new thumbnail
+	tnURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	video.ThumbnailURL = &tnURL
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video with thumbnail URL", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
+	// END: akhdanfadh's implementation of multipart uploads
 }
