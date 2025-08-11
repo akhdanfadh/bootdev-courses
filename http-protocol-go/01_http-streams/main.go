1fe9ac7d8c226f1285708f29fb332b09a415f211
Akhdan Fadhilah <me@akhdan.dev>
2025-08-11 13:58:15 +0900

    [prtc-go] refactor to use channel to return line

--- http-protocol-go/01_http-streams/main.go
+++ http-protocol-go/01_http-streams/main.go
@@ -16,27 +16,41 @@ func main() {
 	}
 	defer file.Close()
 
-	var line string
-	buf := make([]byte, 8)
+	// Consume line from the generated channel
+	lineChan := getLinesChannel(file)
+	for line := range lineChan {
+		fmt.Println("read:", line)
+	}
+}
 
-	for {
-		// Read file 8 bytes at a time
-		n, err := file.Read(buf)
-		if err != nil {
-			if err == io.EOF {
-				break // end-of-file reached
-			} else {
-				log.Fatal(err)
+// getLinesChannel reads lines from a file and returns a channel that emits each line
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	lineChan := make(chan string)
+	go func() {
+		defer close(lineChan) // ensure channel is closed when done
+
+		var line string
+		buf := make([]byte, 8)
+		for {
+			// Read file 8 bytes at a time
+			n, err := file.Read(buf)
+			if err != nil {
+				if err == io.EOF {
+					break // end-of-file reached
+				} else {
+					log.Fatal(err)
+				}
+			}
+
+			// Split buffer into lines
+			parts := strings.Split(string(buf[:n]), "\n")
+			line += parts[0]                  // directly append first part
+			for i := 1; i < len(parts); i++ { // skip first part, process the rest if any
+				line = strings.TrimSuffix(line, "\r") // windows crlf case
+				lineChan <- line                      // send complete line to channel
+				line = parts[i]                       // reset line to next part
 			}
 		}
-
-		// Split buffer into lines
-		parts := strings.Split(string(buf[:n]), "\n")
-		line += parts[0]                  // directly append the first part
-		for i := 1; i < len(parts); i++ { // skip first part, process the rest if any
-			// print current line, windows crlf considered
-			fmt.Println("read:", strings.TrimSuffix(line, "\r"))
-			line = parts[i] // reset line to next part
-		}
-	}
+	}()
+	return lineChan
 }
