829b6f895ff58cf8f96e490473925987dd9945e8
Akhdan Fadhilah <me@akhdan.dev>
2025-08-11 11:31:32 +0900

    [prtc-go] read messages.txt 8 bytes at a time

--- http-protocol-go/01_http-streams/main.go
+++ http-protocol-go/01_http-streams/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	// Open a file for reading
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Read the file 8 bytes at a time
+	for {
+		buffer := make([]byte, 8)
+		if _, err := file.Read(buffer); err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				log.Fatal(err)
+			}
+		}
+		fmt.Println("read:", string(buffer))
+	}
 }
