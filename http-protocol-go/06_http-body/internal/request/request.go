4b29954c12bca92ecb4315be4fc8b6a6a7b922cd
Akhdan Fadhilah <me@akhdan.dev>
2025-08-12 23:50:31 +0900

    [prtc-go] copy chapter 5 to 6

---
+++ http-protocol-go/06_http-body/internal/request/request.go
@@ -0,0 +1,204 @@
+package request
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
+	"unicode"
+
+	"github.com/akhdanfadh/bootdev-courses/http-protocol-go/internal/headers"
+)
+
+// Request represents an HTTP request, based on RFC 9112 Section 2.1.
+type (
+	Request struct {
+		state       parseState
+		RequestLine RequestLine
+		Headers     headers.Headers
+		Body        []byte
+	}
+	RequestLine struct {
+		Method        string
+		RequestTarget string
+		HTTPVersion   string
+	}
+	parseState int
+)
+
+const bufferSize = 8
+const (
+	isDone parseState = iota
+	isRequestLine
+	isHeaders
+	isBody
+)
+
+// RequestFromReader reads an HTTP request from the provided io.Reader.
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	buffer := make([]byte, bufferSize) // buffer to read data into
+	readToIndex := 0                   // keep track how much data we've read
+	request := &Request{
+		state:   isRequestLine,        // initialize the request parse state
+		Headers: headers.NewHeaders(), // initialize headers
+	}
+	for request.state != isDone {
+		// Grow the buffer if full
+		if readToIndex >= len(buffer) {
+			newBuffer := make([]byte, cap(buffer)*2) // double the capacity
+			copy(newBuffer, buffer)
+			buffer = newBuffer
+		}
+
+		// Read into buffer starting at readToIndex
+		bytesRead, err := reader.Read(buffer[readToIndex:])
+		if bytesRead == 0 && err != nil {
+			if err == io.EOF {
+				if request.state != isDone {
+					return nil, fmt.Errorf("incomplete request, currently in state: %v", request.state)
+				}
+				break
+			}
+			return nil, err
+		}
+		readToIndex += bytesRead
+
+		// Parse data we've read so far
+		bytesParsed, err := request.parse(buffer[:readToIndex])
+		if err != nil {
+			return nil, err
+		}
+
+		// Remove data that has been parsed to keep buffer small
+		if bytesParsed > 0 {
+			copy(buffer, buffer[bytesParsed:])
+			readToIndex -= bytesParsed // not forgetting this
+		}
+	}
+
+	// Finally return the request
+	return request, nil
+}
+
+// parse process the data we have so far (previous data + a read from io.Reader).
+func (r *Request) parse(data []byte) (int, error) {
+	totalBytesParsed := 0
+	for r.state != isDone {
+		// - Our parsing function is designed to handle one 'line' at at time,
+		//   by checking if there is a CRLF sequence in the data.
+		// - However, there could be multiple valid parseable parts in given data,
+		//   e.g., `\r\n\r\n` just before request body.
+		// - This parts could lost if we don't parse it again, e.g., if ONLY one
+		//   parse call is done every read AND next read is EOF.
+		// - Thus we need to handle multiple parse calls gracefully here.
+		bytesParsed, err := r.parseSingle(data[totalBytesParsed:])
+		if err != nil {
+			return 0, err
+		}
+		if bytesParsed == 0 {
+			break // need more data to read
+		}
+		totalBytesParsed += bytesParsed
+	}
+	return totalBytesParsed, nil
+}
+
+func (r *Request) parseSingle(data []byte) (int, error) {
+	switch r.state {
+	case isDone:
+		return 0, fmt.Errorf("request is already done")
+	case isRequestLine:
+		requestLine, bytesParsed, err := parseRequestLine(data)
+		if err != nil {
+			return 0, err
+		}
+		if bytesParsed == 0 {
+			return 0, nil
+		} // not enough data to parse request line yet
+		r.RequestLine = *requestLine
+		r.state = isHeaders // move to the next state
+		return bytesParsed, nil
+	case isHeaders:
+		bytesParsed, doneParsing, err := r.Headers.Parse(data)
+		if err != nil {
+			return 0, err
+		}
+		if doneParsing {
+			r.state = isDone
+		} // TODO: for now we only parse request line and headers
+		return bytesParsed, nil
+	case isBody:
+		return 0, fmt.Errorf("parsing body is not implemented yet")
+	default:
+		return 0, fmt.Errorf("unknown parse state: %d", r.state)
+	}
+}
+
+// parseRequestLine parses the request line, based on RFC 9112 Section 3.
+func parseRequestLine(data []byte) (*RequestLine, int, error) {
+	// Only parse if there is CRLF in the data
+	idx := bytes.Index(data, []byte("\r\n"))
+	if idx == -1 {
+		return nil, 0, nil // no CRLF found, nothing to parse, need more data
+	}
+
+	// Split on single space, expecting three parts: method, target, and version
+	line := string(data[:idx])
+	parts := strings.Split(line, " ")
+	if len(parts) != 3 {
+		return nil, 0, fmt.Errorf("invalid request line: %s", line)
+	}
+
+	// Parse validity of each part
+	method, err := parseMethod(parts[0])
+	if err != nil {
+		return nil, 0, err
+	}
+	target, err := parseRequestTarget(parts[1])
+	if err != nil {
+		return nil, 0, err
+	}
+	version, err := parseHTTPVersion(parts[2])
+	if err != nil {
+		return nil, 0, err
+	}
+
+	// Finally, return the parsed RequestLine
+	return &RequestLine{
+		Method:        method,
+		RequestTarget: target,
+		HTTPVersion:   version,
+	}, idx + 2, nil // 2 accounting CRLF bytes
+}
+
+// parseMethod validates the HTTP method, based on RFC 9110 Section 3.1.
+func parseMethod(s string) (string, error) {
+	// TODO: only accept any capitalized word
+	if s == "" {
+		return "", fmt.Errorf("invalid method: %s", s)
+	}
+	for _, r := range s {
+		if !unicode.IsUpper(r) && !unicode.IsLetter(r) {
+			return "", fmt.Errorf("invalid method: %s", s)
+		}
+	}
+	return s, nil
+}
+
+// parseRequestTarget validates the request target, based on RFC 9112 Section 3.2.
+func parseRequestTarget(s string) (string, error) {
+	// TODO: only accept non-empty string
+	if s == "" {
+		return "", fmt.Errorf("invalid request target: %s", s)
+	}
+	return s, nil
+}
+
+// parseHTTPVersion validates the HTTP version, based on RFC 9112 Section 2.3.
+func parseHTTPVersion(s string) (string, error) {
+	// TODO: only accept HTTP/1.1
+	if s != "HTTP/1.1" {
+		return "", fmt.Errorf("invalid HTTP version: %s", s)
+	}
+	return "1.1", nil
+}
---
+++ http-protocol-go/06_http-body/internal/request/request_test.go
@@ -0,0 +1,152 @@
+package request
+
+import (
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// chunkReader is a custom reader struct that simulates reading data in chunks.
+type chunkReader struct {
+	data            string // The source data to read from
+	numBytesPerRead int    // Maximum bytes to read per Read() call
+	pos             int    // Current position in the data (read cursor)
+}
+
+// Read reads up to len(p) or numBytesPerRead bytes from the string per call.
+// It's useful for simulating reading a variable number of bytes per chunk from a network connection.
+// It implements the io.Reader interface, which requires this exact signature.
+func (cr *chunkReader) Read(p []byte) (n int, err error) {
+	if cr.pos >= len(cr.data) { // check if we've already read all data
+		return 0, io.EOF // standard Go convention for end of file
+	}
+	endIndex := min(cr.pos+cr.numBytesPerRead, len(cr.data)) // calculate how far we can read
+	n = copy(p, cr.data[cr.pos:endIndex])                    // number of bytes actually copied to p
+	cr.pos += n                                              // move our reading poisition forward
+	return n, nil                                            // return number of bytes read and nil error
+}
+
+func TestParseRequestLine(t *testing.T) {
+	// Test: Good GET Request line
+	reader := &chunkReader{
+		data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 1,
+	}
+	r, err := RequestFromReader(reader)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HTTPVersion)
+
+	// Test: Good GET Request line with path
+	reader = &chunkReader{
+		data:            "GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 8,
+	}
+	r, err = RequestFromReader(reader)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HTTPVersion)
+
+	// Test: Good POST Request line with path
+	reader = &chunkReader{
+		data:            "POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 1024,
+	}
+	r, err = RequestFromReader(reader)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "POST", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HTTPVersion)
+
+	// Test: Invalid number of parts in request line
+	reader = &chunkReader{
+		data:            "/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 8,
+	}
+	_, err = RequestFromReader(reader)
+	require.Error(t, err)
+
+	// Test: Invalid method (out of order) request line
+	reader = &chunkReader{
+		data:            "/coffee POST HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 8,
+	}
+	_, err = RequestFromReader(reader)
+	require.Error(t, err)
+
+	// Test: Invalid version in Request line
+	reader = &chunkReader{
+		data:            "GET /coffee HTTP/1.0\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 8,
+	}
+	_, err = RequestFromReader(reader)
+	require.Error(t, err)
+}
+
+func TestParseHeaders(t *testing.T) {
+	// Test: Standard Headers
+	reader := &chunkReader{
+		data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 3,
+	}
+	r, err := RequestFromReader(reader)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "localhost:42069", r.Headers["host"])
+	assert.Equal(t, "curl/7.81.0", r.Headers["user-agent"])
+	assert.Equal(t, "*/*", r.Headers["accept"])
+
+	// Test: Empty Headers
+	reader = &chunkReader{
+		data:            "GET / HTTP/1.1\r\n\r\n",
+		numBytesPerRead: 3,
+	}
+	r, err = RequestFromReader(reader)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Empty(t, r.Headers)
+
+	// Test: Malformed Header
+	reader = &chunkReader{
+		data:            "GET / HTTP/1.1\r\nHost localhost:42069\r\n\r\n",
+		numBytesPerRead: 3,
+	}
+	_, err = RequestFromReader(reader)
+	require.Error(t, err)
+
+	// Test: Duplicate Headers
+	reader = &chunkReader{
+		data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nHost: duplicate:8080\r\n\r\n",
+		numBytesPerRead: 3,
+	}
+	r, err = RequestFromReader(reader)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "localhost:42069, duplicate:8080", r.Headers["host"])
+
+	// Test: Case Insensitive Headers
+	reader = &chunkReader{
+		data:            "GET / HTTP/1.1\r\nHOST: localhost:42069\r\nUSER-AGENT: curl/7.81.0\r\n\r\n",
+		numBytesPerRead: 3,
+	}
+	r, err = RequestFromReader(reader)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "localhost:42069", r.Headers["host"])
+	assert.Equal(t, "curl/7.81.0", r.Headers["user-agent"])
+
+	// Test: Missing End of Headers
+	reader = &chunkReader{
+		data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0",
+		numBytesPerRead: 3,
+	}
+	_, err = RequestFromReader(reader)
+	require.Error(t, err)
+}
