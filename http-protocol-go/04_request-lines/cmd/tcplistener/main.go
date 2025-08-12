1a2db9c8477bd728b169584c1314d9cda22ca223
Akhdan Fadhilah <me@akhdan.dev>
2025-08-12 18:06:24 +0900

    [prtc-go] tcp listener using our request package

--- http-protocol-go/04_request-lines/cmd/tcplistener/main.go
+++ http-protocol-go/04_request-lines/cmd/tcplistener/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
+
+	"github.com/akhdanfadh/bootdev-courses/http-protocol-go/internal/request"
 )
 
 func main() {
@@ -27,48 +27,16 @@ func main() {
 		}
 		fmt.Println("Connection has been accepted")
 
-		// Consume line from the connection
-		lineChan := getLinesChannel(conn)
-		for line := range lineChan {
-			fmt.Println(line)
+		// Read the connection
+		req, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal(err)
 		}
+		fmt.Println("Request line:")
+		fmt.Println("- Method:", req.RequestLine.Method)
+		fmt.Println("- Target:", req.RequestLine.RequestTarget)
+		fmt.Println("- Version:", req.RequestLine.HTTPVersion)
+
 		fmt.Println("Connection has been closed")
 	}
-}
-
-// getLinesChannel reads lines from io.ReadCloser interface
-// and returns a channel that emits each line
-func getLinesChannel(rc io.ReadCloser) <-chan string {
-	lineChan := make(chan string)
-	go func() {
-		defer rc.Close()      // ensure the ReadCloser is closed when done
-		defer close(lineChan) // ensure channel is closed when done
-
-		var line string
-		buf := make([]byte, 8)
-		for {
-			// Read file 8 bytes at a time
-			n, err := rc.Read(buf)
-			if err != nil {
-				if line != "" {
-					line = strings.TrimSuffix(line, "\r") // windows crlf case
-					lineChan <- line                      // send any remaining line to channel
-				}
-				if err == io.EOF {
-					break // end-of-file reached
-				}
-				log.Fatal(err)
-			}
-
-			// Split buffer into lines
-			parts := strings.Split(string(buf[:n]), "\n")
-			line += parts[0]                  // directly append first part
-			for i := 1; i < len(parts); i++ { // skip first part, process the rest if any
-				line = strings.TrimSuffix(line, "\r") // windows crlf case
-				lineChan <- line                      // send complete line to channel
-				line = parts[i]                       // reset line to next part
-			}
-		}
-	}()
-	return lineChan
 }
