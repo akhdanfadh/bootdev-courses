c224f4d70d9030ffac66757d7030e18d7732eb93
Akhdan Fadhilah <me@akhdan.dev>
2025-08-13 11:55:36 +0900

    [prtc-go] implement response.Writer and refactor handler around that

--- http-protocol-go/07_http-responses/internal/response/response.go
+++ http-protocol-go/07_http-responses/internal/response/response.go
@@ -21,9 +21,34 @@ var statusText = map[StatusCode]string{
 	StatusInternalServerError: "Internal Server Error",
 }
 
-func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
+type Writer struct {
+	writer io.Writer
+	state  writerState
+}
+type writerState int
+
+const (
+	isStatusLine writerState = iota
+	isHeaders
+	isBody
+)
+
+func NewWriter(w io.Writer) *Writer {
+	return &Writer{
+		writer: w,
+		state:  isStatusLine,
+	}
+}
+
+func (w *Writer) WriteStatusLine(statusCode StatusCode) error {
+	if w.state != isStatusLine {
+		return fmt.Errorf("cannot write status line in state %v", w.state)
+	}
 	statusLine := fmt.Sprintf("HTTP/1.1 %d %s\r\n", statusCode, statusText[statusCode])
-	_, err := w.Write([]byte(statusLine))
+	_, err := w.writer.Write([]byte(statusLine))
+	if err == nil {
+		w.state = isHeaders
+	}
 	return err
 }
 
@@ -35,12 +60,25 @@ func GetDefaultHeaders(contentLen int) headers.Headers {
 	return headers
 }
 
-func WriteHeaders(w io.Writer, headers headers.Headers) error {
+func (w *Writer) WriteHeaders(headers headers.Headers) error {
+	if w.state != isHeaders {
+		return fmt.Errorf("cannot write headers in state %v", w.state)
+	}
 	for key, value := range headers {
-		if _, err := fmt.Fprintf(w, "%s: %s\r\n", key, value); err != nil {
+		if _, err := fmt.Fprintf(w.writer, "%s: %s\r\n", key, value); err != nil {
 			return err
 		}
 	}
-	_, err := w.Write([]byte("\r\n")) // end of headers
+	_, err := w.writer.Write([]byte("\r\n")) // end of headers
+	if err == nil {
+		w.state = isBody
+	}
 	return err
+}
+
+func (w *Writer) WriteBody(p []byte) (int, error) {
+	if w.state != isBody {
+		return 0, fmt.Errorf("cannot write body in state %v", w.state)
+	}
+	return w.writer.Write(p)
 }
--- http-protocol-go/07_http-responses/internal/server/server.go
+++ http-protocol-go/07_http-responses/internal/server/server.go
@@ -1,9 +1,7 @@
 package server
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"strconv"
@@ -19,28 +17,7 @@ type Server struct {
 	isClosed atomic.Bool
 }
 
-type Handler func(w io.Writer, req *request.Request) *HandlerError
-
-type HandlerError struct {
-	StatusCode response.StatusCode
-	Message    string
-}
-
-// Write writes the handler error response to the provided writer.
-func (he *HandlerError) Write(w io.Writer) error {
-	if err := response.WriteStatusLine(w, he.StatusCode); err != nil {
-		return err
-	}
-	messageBytes := []byte(he.Message)
-	headers := response.GetDefaultHeaders(len(messageBytes))
-	if err := response.WriteHeaders(w, headers); err != nil {
-		return err
-	}
-	if _, err := w.Write(messageBytes); err != nil {
-		return err
-	}
-	return nil
-}
+type Handler func(w *response.Writer, req *request.Request)
 
 func Serve(port int, handler Handler) (*Server, error) {
 	if port < 1 || port > 65535 {
@@ -79,39 +56,16 @@ func (s *Server) listen() {
 
 func (s *Server) handle(conn net.Conn) {
 	defer conn.Close() // ensure connection closed after handling
+	w := response.NewWriter(conn)
 
 	// Parse the request from connection
 	req, err := request.RequestFromReader(conn)
 	if err != nil {
-		hErr := &HandlerError{
-			StatusCode: response.StatusBadRequest,
-			Message:    err.Error(),
-		}
-		hErr.Write(conn)
+		w.WriteStatusLine(response.StatusBadRequest)
+		body := fmt.Appendf(nil, "Error parsing request: %v", err)
+		w.WriteHeaders(response.GetDefaultHeaders(len(body)))
+		w.WriteBody(body)
 		return
 	}
-
-	// Handle the request using the provided handler
-	buf := bytes.NewBuffer(nil) // to hold the response body
-	hErr := s.handler(buf, req)
-	if hErr != nil {
-		hErr.Write(conn)
-		return
-	}
-
-	// If all good, write the response
-	if err := response.WriteStatusLine(conn, response.StatusOK); err != nil {
-		log.Println("Error writing status line:", err)
-		return
-	}
-	data := buf.Bytes()
-	headers := response.GetDefaultHeaders(len(data))
-	if err := response.WriteHeaders(conn, headers); err != nil {
-		log.Println("Error writing headers:", err)
-		return
-	}
-	if _, err := conn.Write(data); err != nil {
-		log.Println("Error writing response body:", err)
-		return
-	}
+	s.handler(w, req) // handle if no error
 }
